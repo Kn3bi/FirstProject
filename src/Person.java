a35e66ffe5d92e2c7f85aeaa8585d10003aa0d58
knebi <knb@kkg-do.eu>
2024-09-10 16:56:48 +0200

    Initialer Commit für Repository-Start

---
+++ src/Person.java
@@ -0,0 +1,19 @@
+public class Person {
+
+    // Attribute
+    private String name = "Peter";
+    private int age = 15;
+
+    // Referenzen
+
+    // Konstruktor
+    public Person(){
+        halloSagen();
+    }
+
+    // Methoden
+    public void halloSagen(){
+        System.out.println("Well, hello there!");
+    }
+
+}
